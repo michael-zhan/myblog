ca389a623deab0532377a4b57e4fba9e464ecd9c
rickheart2 <1210257781@qq.com>
2022-07-02 09:44:31 +0800

    7.2

--- ver6.30/js/write.js
+++ ver6.30/js/write.js
@@ -1,3 +1,30 @@
+function bs_input_file() {
+	$(".input-file").before(
+		function() {
+			if ( ! $(this).prev().hasClass('input-ghost') ) {
+				var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+				element.attr("name",$(this).attr("name"));
+				element.change(function(){
+					element.next(element).find('input').val((element.val()).split('\\').pop());
+				});
+				$(this).find("button.btn-choose").click(function(){
+					element.click();
+				});
+				$(this).find("button.btn-reset").click(function(){
+					element.val(null);
+					$(this).parents(".input-file").find('input').val('');
+				});
+				$(this).find('input').css("cursor","pointer");
+				$(this).find('input').mousedown(function() {
+					$(this).parents('.input-file').prev().click();
+					return false;
+				});
+				return element;
+			}
+		}
+	);
+}
+
 $(function(){
     $('.chosen').chosen({
           no_results_text: "没有找到结果！",//搜索无结果时显示的提示  
@@ -6,8 +33,8 @@ $(function(){
           disable_search: false, //禁用搜索。设置为true，则无法搜索选项。
           disable_search_threshold: 0, //当选项少等于于指定个数时禁用搜索。
           inherit_select_classes: true, //是否继承原下拉框的样式类，此处设为继承
-          placeholder_text_single: '选择国家', //单选选择框的默认提示信息，当选项为空时会显示。如果原下拉框设置了data-placeholder，会覆盖这里的值。
-          width: '400px', //设置chosen下拉框的宽度。即使原下拉框本身设置了宽度，也会被width覆盖。
+          placeholder_text_single: '选择分类', //单选选择框的默认提示信息，当选项为空时会显示。如果原下拉框设置了data-placeholder，会覆盖这里的值。
+        //   width: '50%', //设置chosen下拉框的宽度。即使原下拉框本身设置了宽度，也会被width覆盖。
           outerHeight: '50px',
           max_shown_results: 1000, //下拉框最大显示选项数量
           display_disabled_options: false,
@@ -16,4 +43,5 @@ $(function(){
           group_search: false, //选项组是否可搜。此处搜索不可搜
           include_group_label_in_selected: true //选中选项是否显示选项分组。false不显示，true显示。默认false。
      });
+     bs_input_file();
   });
